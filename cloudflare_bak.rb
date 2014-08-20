cbe041c53295710b4c22bb93c59ca075d7f386f0
awatekiran <awatekiran@outlook.com>
2014-08-20 22:46:58 +0530

    added function to save backups to folders created as per date

--- cloudflare_bak.rb
+++ cloudflare_bak.rb
@@ -10,54 +10,51 @@ $apiurl = "https://www.cloudflare.com/api_json.html"
 $token = "adasdasdadasdasdasdasdasdasdasdas"
 $mail = "test@test.com"
 $zonenames_array = []
+$date = Time.new.strftime("%d-%m-%y")
 
-  uri = URI($apiurl)
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  request = Net::HTTP::Post.new(uri.request_uri)
-
-  request.set_form_data({"a" => "zone_load_multi", "tkn" => "#{$token}", "email" => "#{$mail}"})
-
-  response = http.request(request)
-  result = JSON(response.body)
+uri = URI($apiurl)
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+request = Net::HTTP::Post.new(uri.request_uri)
+request.set_form_data({"a" => "zone_load_multi", "tkn" => "#{$token}", "email" => "#{$mail}"})
+response = http.request(request)
+result = JSON(response.body)
 #puts JSON.pretty_generate(result)
-  c=0
-#zonenames_array= []
-  result['response']['zones']['objs'].each do |zone|
-    $zonenames_array[c] = "#{zone['zone_name']}"
-    c += 1
-  end
+c=0
 
-      #print $zonenames_array
-
-def get_records()
-   uri = URI($apiurl)
-   http = Net::HTTP.new(uri.host, uri.port)
-   http.use_ssl = true
-   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-   request = Net::HTTP::Post.new(uri.request_uri)
-      #zonefile = open('zonefile.txt', 'w')
-   FileUtils.cd('/home/test/folder/') do
-      $zonenames_array.each do | domain |
-         testzone = open(domain, 'w')
-         request.set_form_data({"a" => "rec_load_all", "tkn" => "#{$token}", "email" => "#{$mail}", "z" => "#{domain}"})
-         response = http.request(request)
-         result = JSON(response.body)
-    # puts JSON.pretty_generate(result)
-         result['response']['recs']['objs'].each do | entry |
-            if entry['ttl'] == "1"
-               live = "300"
-            else
-               live = entry['ttl']
-            end
-            newentry = entry['name']+" "+live+" IN "+entry['type']+" "+entry['content']
-            testzone.write(newentry)
-            testzone.write("\n")
-         end
-         testzone.close
-      end
-   end
+result['response']['zones']['objs'].each do |zone|
+	$zonenames_array[c] = "#{zone['zone_name']}"
+	c += 1
 end
-
+#print $zonenames_array
+def get_records()
+	uri = URI($apiurl)
+	http = Net::HTTP.new(uri.host, uri.port)
+	http.use_ssl = true
+	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+	request = Net::HTTP::Post.new(uri.request_uri)
+	#zonefile = open('zonefile.txt', 'w')
+	FileUtils.mkdir_p("/backups/folder/#{$date}")
+	FileUtils.cd("/backups/folder/#{$date}") do
+		$zonenames_array.each do | domain |
+			testzone = open(domain, 'w')
+			request.set_form_data({"a" => "rec_load_all", "tkn" => "#{$token}", "email" => "#{$mail}", "z" => "#{domain}"})
+			response = http.request(request)
+			result = JSON(response.body)
+			# puts JSON.pretty_generate(result)
+			result['response']['recs']['objs'].each do | entry |
+				if entry['ttl'] == "1"
+					live = "300"
+				else
+					live = entry['ttl']
+				end
+				newentry = entry['name']+" "+live+" IN "+entry['type']+" "+entry['content']
+				testzone.write(newentry)
+				testzone.write("\n")
+			end
+			testzone.close
+		end
+	end
+end
 get_records()
